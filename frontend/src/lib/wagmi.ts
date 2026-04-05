cb8d374f74429596eb1b5695c9ae96df002f957d
Afolabi Ayomide <afolabiayomide870@gmail.com>
2026-04-06 00:48:21 +0100

    fix: use AppKit built-in celo network type

--- frontend/src/lib/wagmi.ts
+++ frontend/src/lib/wagmi.ts
@@ -1,24 +1,11 @@
 import { cookieStorage, createStorage } from "wagmi"
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi"
-import { defineChain } from "@reown/appkit/networks"
+import { celo } from "@reown/appkit/networks"
+import type { AppKitNetwork } from "@reown/appkit/networks"
 
 export const projectId = "fe23730106f26d938267ccfe7fc56c2f"
 
-export const celo = defineChain({
-  id: 42220,
-  caipNetworkId: "eip155:42220",
-  chainNamespace: "eip155",
-  name: "Celo",
-  nativeCurrency: { name: "CELO", symbol: "CELO", decimals: 18 },
-  rpcUrls: {
-    default: { http: ["https://forno.celo.org"] },
-  },
-  blockExplorers: {
-    default: { name: "Celoscan", url: "https://celoscan.io" },
-  },
-})
-
-export const networks = [celo]
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [celo]
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({ storage: cookieStorage }),
